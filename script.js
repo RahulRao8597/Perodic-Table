f9dd3bc7b6bd1da2167ca4db4d15036414f2b517
kumarbhauak47zindabad <rahulyadav8597@gmail.com>
2024-07-26 23:36:14 +0530

    Perodic-Table

---
+++ script.js
@@ -0,0 +1,32 @@
+const createPeriodicElement = (symbol, atomicNumber, name) => {
+  const element = document.createElement("div");
+  element.classList.add("element");
+  element.innerHTML = symbol;
+
+  element.setAttribute("data-atomic-number", atomicNumber);
+  element.setAttribute("data-element-name", name);
+
+  return element;
+};
+
+const handleElementClick = (event) => {
+  const target = event.target;
+
+  if (target.classList.contains("element")) {
+    const atomicNumber = target.getAttribute("data-atomic-number");
+    const elementName = target.getAttribute("data-element-name");
+    alert(`Atomic number: ${atomicNumber} \n Element name: ${elementName}`);
+  }
+};
+
+const initializePerodicTable = () => {
+  const mainBody = document.querySelector(".mainBody");
+  for (let i = 1; i <= 50; i++) {
+    const element = createPeriodicElement(`Elem${i}`, i, `Element${i}`);
+    mainBody.append(element);
+  }
+
+  mainBody.addEventListener("click", handleElementClick);
+};
+
+initializePerodicTable();
